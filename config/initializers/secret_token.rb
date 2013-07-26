62158c6fd2ed4a181d7cef31af2bfe399fb3a3ed
Donald Plummer <donald.plummer@gmail.com>
2013-07-26 15:27:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HyperGate::Application.config.secret_key_base = '7d55714dde22b169e4457bb075c5480daa7a7ef2a5e280dbc1f322118c465045156bb2f3348e74d33f7cb074c04b18342c26a29e6d093365fa0fd5306cdccab6'
